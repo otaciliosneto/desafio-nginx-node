8de834f5140a072bb21aa03d8928bb00dc9bb75c
Otacilio Neto <otacilio.neto@tjdft.jus.br>
2024-05-10 22:45:41 -0300

    Fix: ajustado comportamento do nginx

--- node/index.js
+++ node/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const random_name = require('node-random-name')
-const mysql = require('mysql2/promise');
 
 const app = express()
+
 const port = 3000
 
 const config = {
@@ -12,47 +12,33 @@ const config = {
     database: 'nodedb'
 };
 
-async function connectToDatabase() {
-  let connection;
+const mysql = require('mysql');
+const connection = mysql.createConnection(config)
 
-  while (!connection) {
-      try {
-          connection = await mysql.createConnection(config);
-      } catch (err) {
-          console.log('Banco de dados indisponível, tentanto novamente em 5 segundos...');
-          await new Promise(resolve => setTimeout(resolve, 5000));
-      }
+const create_table = 'CREATE TABLE IF NOT EXISTS people (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(255) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;'
+connection.query(create_table)
+
+const sql = `INSERT INTO people(name) values ('${random_name()}')`
+connection.query(sql)
+
+const select = 'SELECT name FROM people'
+let list = ''
+connection.query(select, (_err, result, _) => {
+  let i
+  for (i = 0; i < result.length; i++) {
+    list += '<li>' + result[i].name + '</li>'
   }
+})
+connection.end()
 
-  return connection;
-}
+app.get('/', (_req, res) => {
+    let html = '<h1>Full Cycle Rocks!</h1></br>'
+    html += '<ul>'
+    html += list
+    html += '</ul>'
+    res.send(html)
+  })
 
-connectToDatabase().then(async connection => {
-  const create_table = 'CREATE TABLE IF NOT EXISTS people (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, name varchar(200) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8;'
-  try {
-      await connection.query(create_table);
-      const sql = `INSERT INTO people(name) values ('${random_name()}')`
-      await connection.query(sql);
-      const select = 'SELECT name FROM people'
-      const [rows, fields] = await connection.query(select);
-
-      let list = '';
-      for (let i = 0; i < rows.length; i++) {
-          list += '<p> - ' + rows[i].name + '</p>'
-      }
-
-      app.get('/', (_req, res) => {
-          let html = '<h1>Full Cycle Rocks!</h1></br>'
-          html += '<ul>'
-          html += list
-          html += '</ul>'
-          res.send(html)
-      })
-
-      app.listen(port, () => {
-          console.log('Node rodando na porta ' + port)
-      })
-  } catch (err) {
-      console.error(err);
-  }
-});
+app.listen(port, ()=> {
+    console.log('Rodando na porta ' + port)
+})
